9ce408fc44f1da8845c47b1533da62192e29aebe
indinabilah <indinabilahaulia@gmail.com>
2019-05-22 11:05:05 +0700

    model:create

---
+++ models/menu.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Menu = sequelize.define('Menu', {
+    name: DataTypes.STRING,
+    stock: DataTypes.INTEGER,
+    price: DataTypes.INTEGER
+  }, {});
+  Menu.associate = function(models) {
+    // associations can be defined here
+  };
+  return Menu;
+};
