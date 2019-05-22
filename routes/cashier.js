763909da88aaa2f1a398fbda0d098d1aeb1f1c76
Isnan Irsyad <sao.irsyad@gmail.com>
2019-05-22 16:15:36 +0700

    fix: app not starting

--- routes/cashier.js
+++ routes/cashier.js
@@ -1,2 +1,7 @@
 const router = require('express').Router();
 const Cashiers = require('../controllers/cashier')
+
+router.get('/login', (req, res) => {
+  res.send('./cashiers/login.ejs')
+})
+module.exports = router;
