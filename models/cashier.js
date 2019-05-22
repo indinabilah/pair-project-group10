3834644668bd14dbcb9811acac5a2cac86fa23a8
Isnan Irsyad <sao.irsyad@gmail.com>
2019-05-22 20:15:52 +0700

    basic method for register/login

--- models/cashier.js
+++ models/cashier.js
@@ -1,12 +1,24 @@
 'use strict';
+const bcrypt = require('bcryptjs')
+
 module.exports = (sequelize, DataTypes) => {
   const Cashier = sequelize.define('Cashier', {
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
     password: DataTypes.STRING
   }, {});
+
   Cashier.associate = function(models) {
     // associations can be defined here
     Cashier.hasMany(models.Invoice)
   };
+
+  Cashier.addHook('beforeCreate', 'genHashPassword', cashier => {
+    console.log(`Cashier create Hook executed`);
+    const hash = bcrypt.hashSync(cashier.password, 10);
+    cashier.password = hash;
+  })
   return Cashier;
 };
