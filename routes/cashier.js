2484959c16e8b78ddecdcb15dd2642bb13630f79
Isnan Irsyad <sao.irsyad@gmail.com>
2019-05-23 23:10:37 +0700

    ganti layout kasir

--- routes/cashier.js
+++ routes/cashier.js
@@ -1,17 +1,6 @@
 const router = require('express').Router();
 const Cashiers = require('../controllers/cashier')
 
-router.get('/register', Cashiers.registerForm);
-router.post('/register', Cashiers.create);
-router.get('/login', Cashiers.loginForm);
-router.post('/login', Cashiers.login)
-
-router.get('/', Cashiers.getAll)
-router.get('/add', function(req, res){
-    res.render('./cashier/cashier-input.ejs')
-})
-router.post('/add', Cashiers.addCashier)
-router.get('/delete/:id', Cashiers.deleteCashier)
 router.get('/', (req, res, next) => {
   if (req.session.user) next()
   else res.redirect('/cashiers/login')
