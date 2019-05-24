6498f64cee72e3b1408ba5a5c7e9ced59017e17c
Isnan Irsyad <sao.irsyad@gmail.com>
2019-05-24 09:12:27 +0700

    commit controller:

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -5,6 +5,8 @@ const CashModels = Models.Cashier
 const OrderModels = Models.Order
 const MenuModels = Models.Menu
 
+const Cashier = require('../models').Cashier
+
 
 class InvoiceController{
     static getAll(req, res){
@@ -18,21 +20,51 @@ class InvoiceController{
             res.send(err)
         })
     }
+
     static getFindByCus(req, res){
         OrderModels.findAll({
-            where:{
+            
+            where: {
                 CustomerId: req.params.id
-            }
+            },
+            include: [{
+                model: MenuModels
+            }]
         })
         .then(dataCusOrder => {
-            res.render("./transaksi/transaksi.ejs", {
-                dataCusOrder
+            
+           dataCusOrder = dataCusOrder.map(value => value.dataValues)
+                        .map(value => {
+                            value.totalPrice = value.quantity * value.Menu.price
+                            
+                            return value;
+                        })
+                
+            let sum = 0
+            for (let i = 0; i < dataCusOrder.length; i++) {
+                console.log(dataCusOrder[i].totalPrice + sum)
+                sum += +dataCusOrder[i].totalPrice
+            }
+
+            InvModels.create({
+                CustomerId: dataCusOrder[0].CustomerId,
+                CashierId: null,
+                bill: sum,
+                createdAt: new Date,
+                updatedAt: new Date
             })
+            // res.send(dataCusOrder);
+            
+        })
+        .then (() => {
+            res.redirect('/cashiers/')
         })
         .catch(err => {
-            res.send(err)
+          res.send(err);
         })
+      
     }
+
     static getBill(req, res){
         OrderModels.findAll({
             where:{
@@ -40,14 +72,15 @@ class InvoiceController{
             }
         })
         .then(dataCus =>{
-            OrderModels.findAll({
-                where:{
-                    //======= invoice
-                }
-            })
-            res.render("./transaksi/transaksi.ejs", {
+            res.send(dataCus)
+            // OrderModels.findAll({
+            //     where:{
+            //         //======= invoice
+            //     }
+            // })
+            // res.render("./transaksi/transaksi.ejs", {
 
-            })
+            // })
         })
         .catch(err =>{
             res.send(err)
